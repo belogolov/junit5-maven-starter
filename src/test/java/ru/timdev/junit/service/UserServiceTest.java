2daad82cbe2381e5f5c718d55bdeb931a2bb6289
Timur <belogolov.timur@yandex.ru>
2021-07-12 20:20:51 +0300

    Annotations

--- src/test/java/ru/timdev/junit/service/UserServiceTest.java
+++ src/test/java/ru/timdev/junit/service/UserServiceTest.java
@@ -1,14 +1,50 @@
 package ru.timdev.junit.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import ru.timdev.junit.dto.User;
 
+import java.util.List;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeAll
+    static void init() {
+        System.out.println("Before All:");
+    }
+
+    @BeforeEach
+    void prepare() {
+        userService = new UserService();
+        System.out.println("Before each: " + this);
+    }
 
     @Test
     void usersEmptyIfNoUsersAdded() {
-        var userService = new UserService();
+        System.out.println("Test1: " + this);
         var users = userService.getAll();
-        Assertions.assertTrue(users.isEmpty());
+        Assertions.assertTrue(users.isEmpty(), () -> "User list should be empty.");
+    }
+
+    @Test
+    void usersSizeIfUsersAdded() {
+        System.out.println("Test2: " + this);
+        userService.add(new User());
+        userService.add(new User());
+        var users = userService.getAll();
+        Assertions.assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    void deleteDataFromDataBase() {
+        System.out.println("After each: " + this);
+        System.out.println();
+    }
+
+    @AfterAll
+    static void closeConnectionPool() {
+        System.out.println("After All:");
     }
 }
